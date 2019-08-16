aeba9c97d6871a383d79b948176162386fb52066
Basant kumar Hota <basahota@users.noreply.github.com>
2019-08-16 22:09:46 +0530

    #code

---
+++ src/test/java/com/javatechie/spring/cloud/task/api/SpringCloudTaskExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javatechie.spring.cloud.task.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudTaskExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
